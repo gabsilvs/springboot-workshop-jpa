ff390ea618279b084fe6579f125417249c7cf5a5
Gabriel Silvestre <gabrielsilvestre1164@gmail.com>
2025-08-05 14:34:21 -0300

    user entity and resource

---
+++ src/main/java/com/springcourse/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.springcourse.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springcourse.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Gabriel", "gabrielsilvestre1164@gmail.com", "12345", "99999999");
+		return ResponseEntity.ok().body(u);
+		
+	}
+}
